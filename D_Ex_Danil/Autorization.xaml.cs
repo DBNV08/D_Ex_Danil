7996bb183d2ab656ef5a769c0f48d24d72874a60
данил шайхутдинов < @DESKTOP-GH6H69V>
2023-06-15 09:43:00 +0500

    Добавьте файлы проекта.

---
+++ D_Ex_Danil/Autorization.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace D_Ex_Danil
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorization.xaml
+    /// </summary>
+    public partial class Autorization : Window
+    {
+        DExEntities db = new DExEntities();
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            var login = log.Text;
+            var password = pass.Password;
+            var bd = new DExEntities();
+            var user = bd.User.Where(u => u.Login == login && u.Password == password).FirstOrDefault();
+            if (user != null)
+            {
+                new Autorization().Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Данные не верны", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void btn_LoginGhost_Click(object sender, RoutedEventArgs e)
+        {
+            WindowForView win = new WindowForView(null);
+            win.Show();
+            this.Close();
+        }
+    }
+}
---
+++ D_Ex_Danil/Pages/AddProduct.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace D_Ex_Danil.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        DExEntities db = new DExEntities();
+        Product current = new Product();
+        public AddProduct()
+        {
+            InitializeComponent();
+
+            if (product != null)
+            {
+                current = db.Product.Where(w => w.ID_Product == product.ID).ToList().FirstOrDefault();
+            }
+
+            DataContext = current;
+            Producer.ItemsSource = db.Producer.ToList();
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Iamge.Text != "")
+                    current.Image = Iamge.Text;
+                if (Discount.Text != "")
+                    current.Discount = Convert.ToInt32(Discount.Text);
+                if (Producer.SelectedIndex != -1)
+                    current.ID_Producer = (Producer.SelectedItem as Producer).ID_Producer;
+
+                db.Product.AddOrUpdate(current);
+                db.SaveChanges();
+                MessageBox.Show("Данные сохранены успешно!", "Сохранено", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильно ли заполнены все поля", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Count_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private void TextBlock_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private void Price_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+    }
+}
